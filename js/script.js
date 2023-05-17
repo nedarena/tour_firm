a14692f0cba49536f7fb787873df7d83e8d533db
nedarena <neustroeva28032005@gmail.com>
2023-05-17 18:50:21 +0500

    готовый проект

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+});
